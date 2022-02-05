3c00cf108c0778044bcbda8643760baacf21bdeb
Alf Kristian Støyle <20624+stoyle@users.noreply.github.com>
2022-02-05 16:20:38 +0100

    Latest and greatest!

---
+++ docs/precache-manifest.a262adc54a10bef87032e1056d43c39f.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff33349c81121801b3f3a253526053a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdb2a34662c5d5464461",
+    "url": "/static/css/2.498ce522.chunk.css"
+  },
+  {
+    "revision": "e311f881c7961b3a10a2",
+    "url": "/static/css/main.0da36f97.chunk.css"
+  },
+  {
+    "revision": "fdb2a34662c5d5464461",
+    "url": "/static/js/2.63dfc13d.chunk.js"
+  },
+  {
+    "revision": "e311f881c7961b3a10a2",
+    "url": "/static/js/main.6150e732.chunk.js"
+  },
+  {
+    "revision": "ea81da5c3e31dd97e8a9",
+    "url": "/static/js/runtime~main.d09988e9.js"
+  },
+  {
+    "revision": "fada8ec9ed6fb2a4f0ae986b21b230a2",
+    "url": "/static/media/1.fada8ec9.png"
+  },
+  {
+    "revision": "0b0f5803131b0adcd69d2276026d9f41",
+    "url": "/static/media/2.0b0f5803.png"
+  },
+  {
+    "revision": "252841643b2581343ededb5970180a5f",
+    "url": "/static/media/2019-02-15_19.19.29_small.25284164.jpg"
+  },
+  {
+    "revision": "12ada98feaf34444d5433126cc51c5ba",
+    "url": "/static/media/20190626_193827_small.12ada98f.JPG"
+  },
+  {
+    "revision": "27390dfe0b93f5c5e65ba394bf1e974e",
+    "url": "/static/media/2020-04-25_20.42.10_small.27390dfe.jpg"
+  },
+  {
+    "revision": "ee452f740c2f8f3b54a7ae8c7d8f71c6",
+    "url": "/static/media/2021-02-06_14.37.03_small.ee452f74.jpg"
+  },
+  {
+    "revision": "f07fdc92b679b2bc6a7b690ab2115f94",
+    "url": "/static/media/2021-02-06_18.42.35_1_small.f07fdc92.jpg"
+  },
+  {
+    "revision": "ca25a6e9d4fa98d3d28af03dbc84b0a3",
+    "url": "/static/media/2021-03-01_17.18.47_small.ca25a6e9.jpg"
+  },
+  {
+    "revision": "e860e382fd1de6f0be114efb24bdc974",
+    "url": "/static/media/2022-01-31_14.48.38_small.e860e382.jpg"
+  },
+  {
+    "revision": "0db1eecce4a310aeef51b01a801c490d",
+    "url": "/static/media/DSC_0601_small.0db1eecc.JPG"
+  },
+  {
+    "revision": "c4357c8b71f793032322101843c48cac",
+    "url": "/static/media/DSC_0658_small.c4357c8b.JPG"
+  },
+  {
+    "revision": "6b051fb40b4f8ce89f516a7fdc88319f",
+    "url": "/static/media/Gilda_Display-400-latin-ext1.6b051fb4.woff2"
+  },
+  {
+    "revision": "3503694f86f9ccdc14f6d2a3eedb6dde",
+    "url": "/static/media/Gilda_Display-400-latin2.3503694f.woff2"
+  },
+  {
+    "revision": "0758396ba6ef97d039602cfd9f6e5a0c",
+    "url": "/static/media/IMG_1175_small.0758396b.JPG"
+  },
+  {
+    "revision": "fb0a42dd8707452a91ecd3e54ac951aa",
+    "url": "/static/media/IMG_1232_small.fb0a42dd.JPG"
+  },
+  {
+    "revision": "98518f91797ca248597a2dcbbdd1c16a",
+    "url": "/static/media/Jost-300-cyrillic3.98518f91.woff2"
+  },
+  {
+    "revision": "3219ff0593fbb03772753818a232f5c2",
+    "url": "/static/media/Jost-300-latin-ext4.3219ff05.woff2"
+  },
+  {
+    "revision": "bf9077ba6a2896e95ab1ae2263f00dff",
+    "url": "/static/media/Jost-300-latin5.bf9077ba.woff2"
+  },
+  {
+    "revision": "ffcf8cbf3669c80c84dae808ef520f1d",
+    "url": "/static/media/Jost-400-cyrillic6.ffcf8cbf.woff2"
+  },
+  {
+    "revision": "e433cb98692f5127e5bda24774791c4f",
+    "url": "/static/media/Jost-400-latin-ext7.e433cb98.woff2"
+  },
+  {
+    "revision": "ca8179325b0ceeef181c7f2b56b403ad",
+    "url": "/static/media/Jost-400-latin8.ca817932.woff2"
+  },
+  {
+    "revision": "436bc8f42513658e7bd77cb5b5a7d509",
+    "url": "/static/media/Jost-500-cyrillic9.436bc8f4.woff2"
+  },
+  {
+    "revision": "64123fc70c40f7809f6967605b4c9369",
+    "url": "/static/media/Jost-500-latin-ext10.64123fc7.woff2"
+  },
+  {
+    "revision": "b61a79b40f6c12d022d66d6bcf3fde11",
+    "url": "/static/media/Jost-500-latin11.b61a79b4.woff2"
+  },
+  {
+    "revision": "e43dbf9b56feebcb750ba0f60f3fa190",
+    "url": "/static/media/Jost-600-cyrillic12.e43dbf9b.woff2"
+  },
+  {
+    "revision": "3252ff7db08a3d9ff26f4f173401a6c3",
+    "url": "/static/media/Jost-600-latin-ext13.3252ff7d.woff2"
+  },
+  {
+    "revision": "6b2007c8129dceb3c8f3282e0c51e9d2",
+    "url": "/static/media/Jost-600-latin14.6b2007c8.woff2"
+  },
+  {
+    "revision": "e088e380104a2f71fe6f935ab5105499",
+    "url": "/static/media/Jost-700-cyrillic15.e088e380.woff2"
+  },
+  {
+    "revision": "054b22365c9238b64e89f441210c9e4f",
+    "url": "/static/media/Jost-700-latin-ext16.054b2236.woff2"
+  },
+  {
+    "revision": "0ff4ac57e8ac124148e399364c39e018",
+    "url": "/static/media/Jost-700-latin17.0ff4ac57.woff2"
+  },
+  {
+    "revision": "2b1b9291d732c807b74ef995450c3bee",
+    "url": "/static/media/Jost-800-cyrillic18.2b1b9291.woff2"
+  },
+  {
+    "revision": "35ede6b1f36c85585d227089e67a378c",
+    "url": "/static/media/Jost-800-latin-ext19.35ede6b1.woff2"
+  },
+  {
+    "revision": "505b202c92709d320d9fe783289a7039",
+    "url": "/static/media/Jost-800-latin20.505b202c.woff2"
+  },
+  {
+    "revision": "afafb94bc628ec3f312a05c3daa52d5d",
+    "url": "/static/media/P5180345_small.afafb94b.JPG"
+  },
+  {
+    "revision": "6ef881043f90385a55202e261f59220f",
+    "url": "/static/media/anita_og_meg_magnus_bursdag_small.6ef88104.JPG"
+  },
+  {
+    "revision": "45e59b5e12eceab831acc983bae2a833",
+    "url": "/static/media/bg.45e59b5e.jpg"
+  },
+  {
+    "revision": "38e20216a07fd4baffd56b108601ae93",
+    "url": "/static/media/breadcrumb.38e20216.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "6d0891936a661def025d341e5bc10207",
+    "url": "/static/media/error-404.6d089193.svg"
+  },
+  {
+    "revision": "0c127e1f8edff85e3bbe888efb256b9c",
+    "url": "/static/media/flaticon.0c127e1f.woff"
+  },
+  {
+    "revision": "245ef08dbd1654c4de52ec75349595ae",
+    "url": "/static/media/flaticon.245ef08d.woff2"
+  },
+  {
+    "revision": "54603b3dabe009017899c36c3a1758d7",
+    "url": "/static/media/flaticon.54603b3d.eot"
+  },
+  {
+    "revision": "dcf65c4f32fb0fb9382f400de5a6aa3d",
+    "url": "/static/media/flaticon.dcf65c4f.ttf"
+  },
+  {
+    "revision": "e958c5f1ae25f44f53062aaba546e178",
+    "url": "/static/media/flaticon.e958c5f1.svg"
+  },
+  {
+    "revision": "1469791247c0a1112003237aaf056794",
+    "url": "/static/media/fontawesome-webfont.14697912.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "834c886688e286f3ac648629c51fad79",
+    "url": "/static/media/love.834c8866.png"
+  },
+  {
+    "revision": "cc62fc8acf613cb0d67b7c05f695b339",
+    "url": "/static/media/middag_fjord_45174258902_o.cc62fc8a.jpg"
+  },
+  {
+    "revision": "bd1232f5ae0f2586eb102ca11d770760",
+    "url": "/static/media/middag_fjord_45174258902_o_small.bd1232f5.jpg"
+  },
+  {
+    "revision": "f8e61e945bf5848b2cf0c4ade8944a59",
+    "url": "/static/media/page-title.f8e61e94.jpg"
+  },
+  {
+    "revision": "583e8398087867dbe99b6019bf9aa1ae",
+    "url": "/static/media/room.583e8398.png"
+  },
+  {
+    "revision": "4e7d5015dc9e18e4e97df235f05b1ad7",
+    "url": "/static/media/sauna.4e7d5015.png"
+  },
+  {
+    "revision": "05edb709e4b8da561b03906f3b2b3979",
+    "url": "/static/media/shape.05edb709.png"
+  },
+  {
+    "revision": "45b29c77e67e306e2202509cc09596d7",
+    "url": "/static/media/shape2.45b29c77.png"
+  },
+  {
+    "revision": "2305a0aae1ad3be32c9172cbb212d61d",
+    "url": "/static/media/shape3.2305a0aa.png"
+  },
+  {
+    "revision": "d5d9a93dba4ea22558ca63711a92f544",
+    "url": "/static/media/shape4.d5d9a93d.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4a8405343d535e0a60d62d57e95a0563",
+    "url": "/static/media/slide-10.4a840534.jpg"
+  },
+  {
+    "revision": "f15423e4134b50a97c1e5cc4168aeaa7",
+    "url": "/static/media/the-thief_15196595058_o_small.f15423e4.jpg"
+  },
+  {
+    "revision": "23bd008f31500906492e20a4d62e30ed",
+    "url": "/static/media/the-thief_15196666027_o_small.23bd008f.jpg"
+  },
+  {
+    "revision": "23e6934f33ba45b8e7bbec1236c18a1b",
+    "url": "/static/media/the-thief_15383386785_o.23e6934f.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "cba68f986e60df8c74f4a53c3e39595c",
+    "url": "/static/media/themify.cba68f98.svg"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/static/media/themify.e23a7dca.ttf"
+  }
+]);
